bceeb76719cd1dcd5bf1b2b6a40d3d3cab4c7dc4
Fernando Dantas <fernandojrsud@hotmail.com>
2017-01-25 17:46:23 -0300

    Melhorando dashboard

---
+++ js/preloader.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    $('.preloader-background').delay(1700).fadeOut('slow');
+
+    $('.preloader-wrapper')
+        .delay(1700)
+        .fadeOut();
+});
