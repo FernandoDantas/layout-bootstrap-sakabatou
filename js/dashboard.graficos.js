cea9a335bd99df26b99e98781f83c1835a43f13a
Fernando Dantas <fernandojrsud@hotmail.com>
2017-01-26 11:44:24 -0300

    Melhorado Graficose adicionado chart.js

---
+++ js/dashboard.graficos.js
@@ -0,0 +1,66 @@
+var Sakabatou = Sakabatou || {};
+          
+          Sakabatou.GraficoNovosClientes = (function(){
+              
+              function GraficoNovosClientes(){
+                  this.ctx = $('#graficoNovosClientes')[0].getContext('2d');
+              }
+              
+              GraficoNovosClientes.prototype.iniciar = function(){
+                  graficoNovosClientes = new Chart(this.ctx,{
+                      type: 'line',
+                      data: {
+                          labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai'],
+                          datasets: [{
+                                  label: 'Novos Clientes',
+                                  backgroundColor: "rgba(26,179,148,0.5)",
+                                  pointBorderColor: "rgba(26,179,148,1)",
+                                  pointBackgroundColor: "#fff",
+                                  data: [10, 5, 7, 2, 9]                                  
+                          }]
+                      },                      
+                  });
+              }
+              
+              return GraficoNovosClientes;
+              
+            }());
+
+            $(function(){
+                var graficoNovosClientes = new Sakabatou.GraficoNovosClientes();
+                graficoNovosClientes.iniciar();
+            });
+            
+            
+            
+           //Grfaico novas vendas          
+            Sakabatou.GraficoNovasVendas = (function(){
+              
+              function GraficoNovasVendas(){
+                  this.ctx = $('#graficoNovasVendas')[0].getContext('2d');
+              }
+              
+              GraficoNovasVendas.prototype.iniciar = function(){
+                  graficoNovasVendas = new Chart(this.ctx,{
+                      type: 'bar',
+                      data: {
+                          labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai'],
+                          datasets: [{
+                                  label: 'Novas Vendas',
+                                  backgroundColor: "rgba(61, 48, 191, 0.8)",
+                                  pointBorderColor: "rgba(26,179,148,1)",
+                                  pointBackgroundColor: "#fff",
+                                  data: [9, 2, 7, 5, 10]                                  
+                          }]
+                      },                      
+                  });
+              }
+              
+              return GraficoNovasVendas;
+              
+            }());
+
+            $(function(){
+                var graficoNovasVendas = new Sakabatou.GraficoNovasVendas();
+                graficoNovasVendas.iniciar();
+            });      
